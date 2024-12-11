a9fe8350ccf94b4a46d8829f4865c96084519d0d
rttz159 <101782892+rttz159@users.noreply.github.com>
2024-12-11 13:38:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def main():
                     lyrics = data['text']
                     detected_lang = detect(lyrics)
                     translator = Translator()
-                    translated_lyrics = translator.translate(lyrics,src=detected_lang,dest='en')
+                    translated_lyrics = translator.translate(lyrics,src=detected_lang,dest='en').text
                     data['cleaned_lyrics'] = preprocess_text(translated_lyrics)
                     sentiments = []
                     subjectivities = []
@@ -79,7 +79,7 @@ def main():
                 text = user_lyrics
                 detected_lang = detect(text)
                 translator = Translator()
-                translated_lyrics = translator.translate(text,src=detected_lang,dest='en')
+                translated_lyrics = translator.translate(text,src=detected_lang,dest='en').text
                 cleaned_lyrics = preprocess_text(pd.Series([translated_lyrics])).iloc[0]
                 sentiment, subjectivity = analyze_sentiment(cleaned_lyrics)
 
